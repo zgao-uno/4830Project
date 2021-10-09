9cc3f2a8c251a4f1992115484932f616d51e1a5b
zgao <zgao@unomaha.edu>
2021-10-09 21:50:55 +0800

    continue working

---
+++ LibProject/src/main/java/dao/UserDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+public class UserDao {
+	public void login() {
+		
+	}
+	
+	public void register() {
+		
+	}
+	
+	
+	public void updateInfo() {
+		
+	}
+}
---
+++ LibProject/src/main/java/model/User.java
@@ -0,0 +1,47 @@
+package model;
+
+public class User {
+	private String firstName;
+	private String lastName;
+    private String password;
+    private String phone;
+    private String email;
+    private String address;
+    
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
